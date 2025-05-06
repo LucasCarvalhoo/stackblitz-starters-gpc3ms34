775de334f8c3f3a87683b4f4a0126797a4bf62ee
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2025-05-06 14:56:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-gpc3ms34
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/LucasCarvalhoo/stackblitz-starters-gpc3ms34)
---
+++ js/ui.js
@@ -0,0 +1,54 @@
+import { criarUsuarios, listarUsuarios, editarUsuarios, deletarUsuarios } from 'crud.js'
+
+export function setupUI(){
+  document.getElementById('btnCriar').addEventListener('click', () => {
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    
+    if (!nome || !email){
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    criarUsuarios(nome, email);
+    renderLista();
+    document.getElementById('nome').value = '';
+    document.getElementById('email').value = '';
+  });
+  renderLista();
+}
+
+export function renderLista(){
+  const lista = document.getElementById('listaUsuarios');
+  lista.innerHTML = '';
+
+  listarUsuarios(usuarios => {
+    usuarios.forEach(usuario => {
+      const li = document.createElement('li');
+
+      li.innerHTML = `
+      ${usuario.nome} (${usuario.email})
+      <button onclick="editar(${usuario.id})">Editar</button>
+      <button onclick="excluir(${usuario.id})">Excluir</button>
+      `;
+      lista.appendChild(li);
+    })
+  })
+}
+
+window.editar = (id) => {
+  const novoNome = prompt("Novo nome");
+  const novoEmail = prompt("Novo email");
+
+  if(novoNome && novoEmail){
+    editarUsuarios(id, novoNome, novoEmail);
+    renderLista();
+  }
+}
+
+window.excluir = (id) => {
+  if(confirm("Tu tem certeza?")){
+    deletarUsuarios(id);
+    renderLista();
+  }
+}
